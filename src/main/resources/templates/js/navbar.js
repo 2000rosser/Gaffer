8081b9961a60bd89ceba3e39a1ee4678cd4746ca
Ross Murphy <RossMurphy@ibm.com>
2024-09-11 21:08:04 +0100

    Register page design
    Signed-off-by: Ross Murphy <RossMurphy@ibm.com>

--- src/main/resources/templates/js/navbar.js
+++ src/main/resources/templates/js/navbar.js
@@ -2,17 +2,17 @@ function toggleDropdown() {
     var dropdown = document.getElementById("profileDropdown");
     dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
 
-    // var icon = document.getElementById("profileicon")
-    // icon.style.boxShadow = "1px 1px 10px rgba(0, 0, 0, 0.6)"
-    
-    
     var icon = document.getElementById("profileicon")
-    icon.addEventListener("mouseenter", function( event ) {   
-        event.target.style.boxShadow = "1px 1px 10px rgba(0, 0, 0, 0.6)";
-    }, false);
-    icon.addEventListener("mouseleave", function( event ) {   
-        event.target.style.boxShadow = "";
-    }, false);
+    icon.style.boxShadow = "1px 1px 10px rgba(0, 0, 0, 0.6)"
+    
+    
+    // var icon = document.getElementById("profileicon")
+    // icon.addEventListener("mouseenter", function( event ) {   
+    //     event.target.style.boxShadow = "1px 1px 10px rgba(0, 0, 0, 0.6)";
+    // }, false);
+    // icon.addEventListener("mouseleave", function( event ) {   
+    //     event.target.style.boxShadow = "";
+    // }, false);
 }
 
 window.onclick = function(event) {
@@ -21,7 +21,7 @@ window.onclick = function(event) {
         var icon = document.getElementById("profileicon")
         if (dropdown && dropdown.style.display === "block") {
             dropdown.style.display = "none";
-            icon.style.boxShadow = "none"
+            // icon.style.boxShadow = "none"
         }
     }
 }
