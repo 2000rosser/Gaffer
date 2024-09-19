fbb611d41f785dee4fb076b134b3145ea50f9e60
Ross Murphy <RossMurphy@ibm.com>
2024-09-19 21:28:49 +0100

    clean listing repository
    Signed-off-by: Ross Murphy <RossMurphy@ibm.com>

--- src/main/java/com/example/gaffer/repositories/ListingRepository.java
+++ src/main/java/com/example/gaffer/repositories/ListingRepository.java
@@ -1,22 +1,9 @@
 package com.example.gaffer.repositories;
 
-import java.util.List;
-
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.example.gaffer.models.Listing;
 
 public interface ListingRepository extends JpaRepository<Listing, String> {
 
-    // @Query("SELECT l.name FROM LocationEntity l WHERE LOWER(l.name) LIKE %:term%")
-    // List<String> findByNameContaining(@Param("term") String term, Pageable pageable);
-
-    // @Query("SELECT l.bigName FROM LocationEntity l WHERE l.bigName LIKE %:term%")
-    // List<String> findByBigNameContaining(@Param("term") String term, Pageable pageable);
-
-    // @Query("SELECT l FROM LocationEntity l WHERE TRIM(l.name) = TRIM(:name)")
-    // LocationEntity findByName(@Param("name") String name);
 }
