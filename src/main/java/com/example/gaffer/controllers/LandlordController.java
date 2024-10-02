516de7519ed0e4428e7a5a9aac80f44833c952b5
Ross Murphy <RossMurphy@ibm.com>
2024-10-02 21:18:16 +0100

    /listings/create idea
    Signed-off-by: Ross Murphy <RossMurphy@ibm.com>

--- src/main/java/com/example/gaffer/controllers/LandlordController.java
+++ src/main/java/com/example/gaffer/controllers/LandlordController.java
@@ -1,7 +1,14 @@
 package com.example.gaffer.controllers;
 
+import org.apache.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.gaffer.models.Listing;
+import com.example.gaffer.models.UserEntity;
 
 @Controller
 public class LandlordController {
@@ -14,6 +21,15 @@ public class LandlordController {
     @GetMapping("/listing-management")
     public String getListingManagement(){
         return "listing-management";
+    }
+
+    @PostMapping("/listings/create")
+    public int createListing(@ModelAttribute("listing") Listing listing, Authentication authentication){
+        // TODO make a new listing object
+        UserEntity user = (UserEntity) authentication.getPrincipal();
+        listing.setId(String.valueOf(user.getId()));
+        
+        return HttpStatus.SC_OK;
     }
 
 }
